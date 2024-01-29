4b292703b9d5a559c099ef8f0d55bcd21b7bd83d
kawamura <ayabomax2003@gmail.com>
2024-01-29 11:13:18 +0900

    直した

--- Web/check.php
+++ Web/check.php
@@ -11,11 +11,35 @@ if (!isset($_SESSION['join'])) {
 if (!empty($_POST['check'])) {
     // パスワードを暗号化
     $hash = password_hash($_SESSION['join']['password'], PASSWORD_BCRYPT);
+    
+    $statement = $db->prepare("INSERT INTO family SET family_name=?");
+    $statement->execute(array(
+        $_SESSION['join']['family_name'],
+    ));
  
+    $statement = $db->prepare('SELECT * FROM family WHERE family_name=?');
+    $statement->execute(array(
+        $_SESSION['join']['family_name'],
+    ));
+
+    $record = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($record !== false) {
+        end($record);
+        $family_id = $record['family_id'];
+    } else {
+        $family_id = null;
+        error_log('Fetch failed in check.php');
+    }
+    $family_id = $record['family_id'];
+
     // 入力情報をデータベースに登録
     $statement = $db->prepare(
+        // "INSERT INTO user 
+        // SET username=?, password=?, first_name=?, last_name=?, birthday=?, gender_id=?, role_id=?, savings=?, family_id"
         "INSERT INTO user 
-        SET username=?, password=?, first_name=?, last_name=?, birthday=?, gender_id=?, role_id=?, savings=?, family_id=?"
+        (username, password, first_name, last_name, birthday, gender_id, role_id, savings, family_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
     );
     $statement->execute(array(
         $_SESSION['join']['username'],
@@ -26,7 +50,7 @@ if (!empty($_POST['check'])) {
         $_SESSION['join']['gender_id'],
         $_SESSION['join']['role_id'],
         $_SESSION['join']['savings'],
-        $_SESSION['join']['family_id']
+        $family_id
     ));
 
  
@@ -80,6 +104,11 @@ if (!empty($_POST['check'])) {
             <div class="control">
                 <p>手持ち金額</p>
                 <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['savings'], ENT_QUOTES); ?></span></p>
+            </div>
+
+            <div class="control">
+                <p>家族名</p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['family_name'], ENT_QUOTES); ?></span></p>
             </div>
             
             <br>

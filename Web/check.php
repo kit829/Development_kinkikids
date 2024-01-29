63f82835bd57d3e5424fbbef0a2eb38364bfde69
kawamura <ayabomax2003@gmail.com>
2024-01-29 12:03:14 +0900

    若干関数化

--- Web/check.php
+++ Web/check.php
@@ -1,59 +1,18 @@
 <?php
 require("./dbconnect.php");
+require("./check_function.php");
 session_start();
- 
+
 /* 会員登録の手続き以外のアクセスを飛ばす */
 if (!isset($_SESSION['join'])) {
     header('Location: entry.php');
     exit();
 }
- 
+
 if (!empty($_POST['check'])) {
-    // パスワードを暗号化
-    $hash = password_hash($_SESSION['join']['password'], PASSWORD_BCRYPT);
-    
-    $statement = $db->prepare("INSERT INTO family SET family_name=?");
-    $statement->execute(array(
-        $_SESSION['join']['family_name'],
-    ));
- 
-    $statement = $db->prepare('SELECT * FROM family WHERE family_name=?');
-    $statement->execute(array(
-        $_SESSION['join']['family_name'],
-    ));
-
-    $record = $statement->fetch(PDO::FETCH_ASSOC);
-
-    if ($record !== false) {
-        end($record);
-        $family_id = $record['family_id'];
-    } else {
-        $family_id = null;
-        error_log('Fetch failed in check.php');
-    }
-    $family_id = $record['family_id'];
-
-    // 入力情報をデータベースに登録
-    $statement = $db->prepare(
-        "INSERT INTO user 
-        (username, password, first_name, last_name, birthday, gender_id, role_id, savings, family_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    );
-    $statement->execute(array(
-        $_SESSION['join']['username'],
-        $hash,
-        $_SESSION['join']['first_name'],
-        $_SESSION['join']['last_name'],
-        $_SESSION['join']['birthday'],
-        $_SESSION['join']['gender_id'],
-        $_SESSION['join']['role_id'],
-        $_SESSION['join']['savings'],
-        $family_id
-    ));
-
- 
+    checkUser($db, $_SESSION['join']);
     unset($_SESSION['join']);   // セッションを破棄
-    header('Location: thank.php');   // thank.phpへ移動
+    header('Location: thank.php');
     exit();
 }
 ?>
@@ -85,13 +44,13 @@ if (!empty($_POST['check'])) {
             </div>
  
             <div class="control">
-                <p>名前</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['first_name'], ENT_QUOTES); ?></span></p>
+                <p>名字</p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['last_name'], ENT_QUOTES); ?></span></p>
             </div>
 
             <div class="control">
-                <p>名字</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['last_name'], ENT_QUOTES); ?></span></p>
+                <p>名前</p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['first_name'], ENT_QUOTES); ?></span></p>
             </div>
 
             <div class="control">

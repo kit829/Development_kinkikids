8250f3ba305857e9793cda173e995c285ee116d4
takegoshi54 <fki2266215@gmail.com>
2024-01-30 12:09:04 +0900

    takegoshi

---
+++ web/sein.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if(isset($_POST["id"])&isset($_POST["id"])){
+    $_SESSION["user_id"] = $_POST["id"];
+    $_SESSION["family_id"] = $_POST["fid"];
+    echo "ユーザーID=".$_POST["id"];
+    echo "<br>";
+    echo "家族ID=".$_POST["fid"];
+    echo "<br>";
+}
+
+?>
+<form action="" method="post">
+    ユーザーID<input type="int" name="id" value="8"><br>
+    家族ID<input type="int" name="fid" value="8"><br>
+    <button type = "submit">ID変更</button>
+</form>

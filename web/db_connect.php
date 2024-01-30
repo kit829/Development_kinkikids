604a89773a761861b4c6af6e80f2ae04971b1523
takegoshi54 <fki2266215@stu.o-hara.ac.jp>
2024-01-30 10:44:25 +0900

    hatanakaとnakajimaマージ

--- web/db_connect.php
+++ web/db_connect.php
@@ -1,44 +1,21 @@
 <?php
- class connect{
-  const DB_NAME = "LAA1579749-kinkikids";
-  const HOST = "mysql212.phy.lolipop.lan";
-  const USER = "LAA1579749";
-  const PASS = "kinkikidsPass";
+// test
+class connect extends PDO {
+    const DB_NAME = "kinkikids";
+    const HOST = "127.0.0.1";
+    const USER = "root";
+    const PASS = "";
 
-  private $dbh;
+    public function __construct(){
+        $dsn = "mysql:host=".self::HOST.";dbname=".self::DB_NAME.";charset=utf8mb4";
+        try {
+            // 親クラスのコンストラクタを呼び出す
+            parent::__construct($dsn, self::USER, self::PASS);
+        } catch(Exception $e){
+            exit($e->getMessage());
+        }
 
-  public function __construct(){
-    $dsn = "mysql:host=".self::HOST.";dbname=".self::DB_NAME.";charset=utf8mb4";
-    try {
-      // PDOのインスタンスをクラス変数に格納する
-      $this->dbh = new PDO($dsn, self::USER, self::PASS);
-
-    } catch(Exception $e){
-      // Exceptionが発生したら表示して終了
-      exit($e->getMessage());
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
-
-    // DBのエラーを表示するモードを設定
-    $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-  }
-
-  public function query($sql, $param = null){
-    // プリペアドステートメントを作成し、SQL文を実行する準備をする
-    $stmt = $this->dbh->prepare($sql);
-    // パラメータを割り当てて実行し、結果セットを返す
-    $stmt->execute($param);
-    return $stmt;
-  }
-
-    // try {
-    //     $pdo = new PDO(DB_NAME,HOST,USER,PASS,[
-    //         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    //         PDO::ATTR_EMULATE_PREPARES =>false
-    //     ]);
-    // } catch (PDOException $e) {
-    //     echo 'ERROR: Could not connect.'.$e->getMessage()."\n";
-    //     exit();
-    // }
 }
 ?>
